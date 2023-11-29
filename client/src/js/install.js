17cc5b92191329713782695ee62d41b49b7074f6
evillarreal18 <ericvillarreal81997@gmail.com>
2023-11-28 20:53:35 -0600

    finished up some code in the routes folder

---
+++ client/src/js/install.js
@@ -0,0 +1,16 @@
+const butInstall = document.getElementById('buttonInstall');
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    butInstall.style.visibility = 'visible';
+    butInstall.textContent = 'Install!'
+});
+
+butInstall.addEventListener('click', async () => {
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = 'Installed!';
+});
+
+window.addEventListener('appinstalled', (event) => {
+    console.log('😇', 'appinstalled', event);
+});
